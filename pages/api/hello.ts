ad67c517e174bdeac4e5fad670178643b491f09a
Garrett Cox <garrettjcox@gmail.com>
2020-11-25 13:56:13 -0600

    Add eslint/prettier

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  res.statusCode = 200;
+  res.json({ name: 'John Doe' });
+};
 
-export default handler
+export default handler;
