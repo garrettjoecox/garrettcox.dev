0f365cee55171afc81f66a16b38ce945350f45d3
Garrett Cox <garrettjcox@gmail.com>
2020-12-05 22:20:42 -0600

    Add some meta website stuff

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Garrett Cox</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+          <meta name="google-site-verification" content="QKaBf-q9jtSoWsWHrzudNrFiJ70f3xbvvpbASCjtNdQ" />
+          <meta name="author" content="Garrett Cox" />
+          <meta name="description" content="Garrett Cox's Personal Website" />
+          <meta name="keywords" content="Garrett Joe Cox personal website github linkedin" />
+          <meta property="og:title" content="Garrett Cox" />
+          <meta property="og:description" content="Garrett Cox's Personal Website" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="http://garrettcox.dev/" />
+          <meta property="og:image" content="http://garrettcox.dev/og.png" />
+          <link href="/favicon.ico" rel="shortcut icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
